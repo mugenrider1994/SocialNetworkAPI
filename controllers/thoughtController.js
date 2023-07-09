53dc9159a6c8671e3577522db506ef053acad13b
Megan Glorioso <meganglorioso2016@gmail.com>
2023-07-09 15:28:11 -0700

    finishing touches

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -27,7 +27,7 @@ const createThought = async (req, res) => {
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(400).json({ message: 'No user found with this Id!' });
+      return res.status(400).json({ message: 'No user found using this Id!' });
     }
     const thought = await Thought.create({ thoughtText, username, userId });
     user.thoughts.push(thought._id);
@@ -47,7 +47,7 @@ const updateThought = async (req, res) => {
       { new: true, runValidators: true }
     );
     if (!thought) {
-      return res.status(400).json({ message: 'No thought found with this Id!' });
+      return res.status(400).json({ message: 'No thought found!' });
     }
     res.json(thought);
   } catch (error) {
@@ -60,12 +60,12 @@ const deleteThought = async (req, res) => {
   try {
     const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
     if (!thought) {
-      return res.status(400).json({ message: 'No thought found with this Id!' });
+      return res.status(400).json({ message: 'No thought found!' });
     }
     const user = await User.findOne({ username: thought.username });
     user.thoughts.pull(thought._id);
     await user.save();
-    res.json({ message: 'Thought deleted successfully!' });
+    res.json({ message: 'Deleted successfully!' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server Error' });
